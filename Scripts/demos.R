cf4b08c716908406173e0ec08764b2001d37c96c
Weihs <wei115@csiro.au>
2019-09-19 12:52:40 +1000

    playing around

---
+++ Scripts/demos.R
@@ -0,0 +1,26 @@
+# SOME PLAYING AROUND WITH MATHEMATICAL FUNCTIONS, EQUALITY AND VARIABLES
+x <- 1 + 100 / 5^4
+y <- 1
+x / y
+# EQUAL?
+if (x > y){
+  print ("x is bigger than y")
+} 
+else {
+  print("y is bigger than x")
+  }
+
+x <- NA
+is.na(x)
+# VECTORS
+z <- 1:5
+z + 5
+z <- c(4,7,3,8,9234,3)
+# VARIABLE NAMING SYSTEM and DELETING VARIABLES
+2minheight <- 5
+_age <- 5
+.mass <- 5
+ls(all.names = TRUE)
+rm(min_height)
+3^(10:50)
+ls()

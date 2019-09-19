8dd569bad373b6b8af7bc984e77264dcf2461f26
Weihs <wei115@csiro.au>
2019-09-19 16:32:25 +1000

    Calculator command practices

--- Scripts/demos.R
+++ Scripts/demos.R
@@ -1,8 +1,7 @@
-# SOME PLAYING AROUND WITH MATHEMATICAL FUNCTIONS, EQUALITY AND VARIABLES
+#1 SOME PLAYING AROUND WITH MATHEMATICAL FUNCTIONS, EQUALITY AND VARIABLES
 x <- 1 + 100 / 5^4
 y <- 1
 x / y
-# EQUAL?
 if (x > y){
   print ("x is bigger than y")
 } 
@@ -12,11 +11,13 @@ else {
 
 x <- NA
 is.na(x)
-# VECTORS
+
+#2 VECTORS
 z <- 1:5
 z + 5
 z <- c(4,7,3,8,9234,3)
-# VARIABLE NAMING SYSTEM and DELETING VARIABLES
+
+#3 VARIABLE NAMING SYSTEM and DELETING VARIABLES
 2minheight <- 5
 _age <- 5
 .mass <- 5
@@ -24,3 +25,11 @@ ls(all.names = TRUE)
 rm(min_height)
 3^(10:50)
 ls()
+installed.packages()
+installed.packages("tidyverse")
+
+#4 Dataframes
+read.csv("data/gapminder.csv")
+library(tidyverse)
+read_csv("data/gapminder.csv")
+read.csv("data/gapminder.csv")
